fef13b6b238be46047c8f6fb94c3c5dddf8a585e
Rahullohar703 <127471119+Rahullohar703@users.noreply.github.com>
2024-12-20 13:51:34 +0530

    made some new changes

--- src/menu links/Contact.js
+++ src/menu links/Contact.js
@@ -1,8 +1,6 @@
 // src/menu links/Contact.js
 import React from 'react';
 import { motion } from 'framer-motion';
-// @ts-ignore
-import FormfacadeEmbed from '@formfacade/embed-react';
 import './Contact.css'; // Import optional CSS for styling
 
 const Contact = () => {
@@ -21,12 +19,20 @@ const Contact = () => {
         Contact Us
       </motion.h1>
 
-      {/* Embed the Form using FormfacadeEmbed */}
+      {/* Embed the Google Form using an iframe */}
       <div className="form-wrapper">
-        <FormfacadeEmbed
-          formFacadeURL="https://formfacade.com/include/101808954562389489787/form/1FAIpQLSdGNxu58HTL71RQ5t-YnKQJXaMkwW3Hem0KeSk7zxP9epso1A/classic.js/?div=ff-compose"
-          onSubmitForm={() => console.log('Form submitted')}
-        />
+      <iframe
+  src="https://docs.google.com/forms/d/e/1FAIpQLSdGNxu58HTL71RQ5t-YnKQJXaMkwW3Hem0KeSk7zxP9epso1A/viewform?embedded=true"
+  width="100%"
+  height="800"
+  frameBorder="0"
+  marginHeight="0"
+  marginWidth="0"
+  title="Google Form"
+>
+  Loading…
+</iframe>
+
       </div>
     </motion.div>
   );
